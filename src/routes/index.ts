c1e41de2ec7d3203811aa1e946e24388d7488a09
Geoff Rich <4992896+geoffrich@users.noreply.github.com>
2022-07-09 18:20:38 -0700

    Remove faker

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,4 @@
 import type { RequestHandler } from './__types/index';
-import { faker } from '@faker-js/faker';
 
 const CITY_HEADER = 'x-vercel-ip-city';
 const COUNTRY_HEADER = 'x-vercel-ip-country';
@@ -7,16 +6,10 @@ const COUNTRY_HEADER = 'x-vercel-ip-country';
 export const get: RequestHandler = function ({ request }) {
 	let current_city = '';
 	let visited: string[] = [];
-	if (import.meta.env.DEV) {
-		current_city = getFakeCity();
-		visited = Array(5)
-			.fill('')
-			.map((_) => getFakeCity());
-	} else {
-		const city = request.headers.get(CITY_HEADER) ?? 'unknown city';
-		const country = getCountryName(request.headers.get(COUNTRY_HEADER));
-		current_city = `${city}, ${country}`;
-	}
+
+	const city = request.headers.get(CITY_HEADER) ?? 'unknown city';
+	const country = getCountryName(request.headers.get(COUNTRY_HEADER));
+	current_city = `${city}, ${country}`;
 
 	return {
 		body: {
@@ -25,10 +18,6 @@ export const get: RequestHandler = function ({ request }) {
 		}
 	};
 };
-
-function getFakeCity() {
-	return `${faker.address.cityName()}, ${getCountryName(faker.address.countryCode())}`;
-}
 
 const displayNames = new Intl.DisplayNames(['en'], { type: 'region' });
 function getCountryName(countryCode: string | null) {
