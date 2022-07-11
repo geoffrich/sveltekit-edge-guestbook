90f51503d3a5cfe887f3d848e9c9a38d96fc6146
Geoff Rich <4992896+geoffrich@users.noreply.github.com>
2022-07-10 20:53:27 -0700

    Add comment

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,10 @@ import adapter from '@sveltejs/adapter-vercel';
 import preprocess from 'svelte-preprocess';
 import dotenv from 'dotenv';
 
+// this is here so it only runs in dev
+// we need process.env available for the Upstash environment variables
+// and we can't use VITE_ variables since we don't want to expose the
+// connection string client side
 dotenv.config();
 
 /** @type {import('@sveltejs/kit').Config} */
