e8fdaca53292c9f2778937e51297ccc4e7992552
Geoff Rich <4992896+geoffrich@users.noreply.github.com>
2022-07-17 15:39:10 -0700

    Update default city

--- src/hooks.ts
+++ src/hooks.ts
@@ -3,8 +3,8 @@ import { COUNTRY_HEADER, CITY_HEADER } from '$lib/constants';
 
 export const handle: Handle = async function ({ event, resolve }) {
 	if (import.meta.env.DEV) {
-		event.request.headers.append(CITY_HEADER, 'Seattle');
-		event.request.headers.append(COUNTRY_HEADER, 'US');
+		event.request.headers.append(CITY_HEADER, 'Kakariko Village');
+		event.request.headers.append(COUNTRY_HEADER, 'Hyrule');
 	}
 	const response = await resolve(event);
 	return response;
