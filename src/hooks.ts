d8e7222ea8ef0347f491b11951bbf5f616a32095
Geoff Rich <4992896+geoffrich@users.noreply.github.com>
2022-07-17 17:05:04 -0700

    Refactor

--- src/hooks.ts
+++ src/hooks.ts
@@ -4,7 +4,7 @@ import { COUNTRY_HEADER, CITY_HEADER } from '$lib/constants';
 export const handle: Handle = async function ({ event, resolve }) {
 	if (import.meta.env.DEV) {
 		event.request.headers.append(CITY_HEADER, 'Kakariko Village');
-		event.request.headers.append(COUNTRY_HEADER, 'Hyrule');
+		event.request.headers.append(COUNTRY_HEADER, 'JP');
 	}
 	const response = await resolve(event);
 	return response;
